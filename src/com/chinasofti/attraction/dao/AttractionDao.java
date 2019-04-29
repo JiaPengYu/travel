4e16f48f4d81065eb28fad4377872b77750a6bf9
LGanlan <48249467+LGanlan@users.noreply.github.com>
2019-04-30 00:05:47 +0800

    Update AttractionDao.java

--- src/com/chinasofti/attraction/dao/AttractionDao.java
+++ src/com/chinasofti/attraction/dao/AttractionDao.java
@@ -1,11 +1,7 @@
 package com.chinasofti.attraction.dao;
 
 import com.chinasofti.attraction.entity.Attraction;
-import com.chinasofti.base.BaseDao;
-
-
-public interface AttractionDao extends BaseDao<Attraction> {
-    
+import com.chinasofti.base.BaseDao;    
 import java.util.List;
 
 public interface AttractionDao extends BaseDao<Attraction> {
